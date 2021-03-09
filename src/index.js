aa1d42d42e4d64d868050123869cc5458431e045
spiderVS <fuzzy003@gmail.com>
2021-03-09 22:35:43 +0300

    feat: task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let number = Math.abs(n);
+    let numberToStr = number +'';
+    let newStr = '';
+    for (let i = (numberToStr.length - 1); i >= 0; i--) {
+        newStr += numberToStr[i];
+    }
+    number = +newStr;
+return number;
 }
